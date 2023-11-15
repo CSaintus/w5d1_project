cab57596c8ca1c61db0bd985067baf133e249b9a
Codylive <144850978+Codylive@users.noreply.github.com>
2023-11-15 00:35:22 -0500

    w5d1 project

---
+++ insert_movie_theater.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO Customers (customer_name, email, phone) VALUES
+('Pele Simmon', 'pele@simmon.com', '987-654-3210');
+
+
+
+INSERT INTO Movies (title, date_released, rating, genre) VALUES
+('L'eau dans la riviere', '2023-10-10', 'PG-13', 'Ation');
+
+
+INSERT INTO Tickets (customer_id, movies_id, seat, price, ticket_date) VALUES
+(1, 1, 'C07', 7.85, '2023-15-05'),
+
+
+
+INSERT INTO Concessions (customer_id, concessioned_value, concessioned_article) VALUES
+(1, '3.50', 'Hotdog'),
+
